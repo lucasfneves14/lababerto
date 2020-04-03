1115ef87ab05f5536827b0fcbe2ebf0b73f88c16
lucas <lucasfneves14@gmail.com>
2020-04-03 17:26:54 -0300

    first commit

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,7 +4,8 @@ CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS',
       :aws_access_key_id => 'AKIAIBE7AR7LCRRTWWNQ',
-      :aws_secret_access_key => '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'
+      :aws_secret_access_key => 'gpUFERVfMlGry4w0GLMIm6VwpNmteOuO1pCd10hA',
+      :region => 'sa-east-1'
     }
     config.fog_directory = 'lababertobsb'
     config.fog_attributes = {'Cache-Control' => 'max-age=3153600', 'Expires' => 'max'}
